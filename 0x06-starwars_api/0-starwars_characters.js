e45ba1b74c69bd98d1397a345eef69b1b264e6e7
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2024-04-19 02:52:50 +0200

    star war actors styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,17 +3,17 @@
 const request = require('request');
 
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2],
-        function (err, res, body) {
+  function (err, res, body) {
     if (err) throw err;
     const actors = JSON.parse(body).characters;
     printing(actors, 0);
-});
+  });
 
 const printing = (actors, x) => {
-    if (x === actors.length) return;
-    request(actors[x], function (err, res, body) {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
     printing(actors, x + 1);
-    });
+  });
 };
