80823be3644fe471a92be0050be1499d0a606288
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2024-04-19 02:42:46 +0200

    star war actors edit long line

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2],
-    function (err, res, body) {
+        function (err, res, body) {
     if (err) throw err;
     const actors = JSON.parse(body).characters;
     printing(actors, 0);
