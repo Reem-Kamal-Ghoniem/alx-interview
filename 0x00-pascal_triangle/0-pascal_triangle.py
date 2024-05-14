815595ee5a127b15545270bf98e39e4805899fc7
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2024-05-15 01:15:58 +0200

    pascal time

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+file for Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """Create a function def pascal_triangle
+    """
+    result = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            Count = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                Count = Count * (i - j) // j
+            result.append(level)
+    return result
